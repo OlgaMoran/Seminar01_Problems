1c6d047ddcb79ed9a959713eee5fbc77f24fb106
Olga Moran <olga@azimuth.co.za>
2022-04-16 21:52:05 +0800

    Added Promlem 1 and 2

---
+++ Problem_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает три числа, а на выходе выдает максимальное из этих чисел.
+int a = 55;
+int b = 7;
+int c = 84562;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else
+{
+    if (c > max)
+    {
+        max = c;
+    }
+}
+Console.Write("max = ");
+Console.WriteLine(max);
