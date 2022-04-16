6aaee83a64f7a8a33dccf79fdc0b6cc29c672995
Olga Moran <olga@azimuth.co.za>
2022-04-16 23:28:49 +0800

    Added Problem 4 (in progress)

---
+++ Problem_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+// for (int x = 2; x <= 8; x += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+int N = 5;
+int x = 2;
+while(x >= N)
+{
+    Console.WriteLine(x);
+    x = x + 2;
+    return;
+}
+Console.WriteLine(x);
